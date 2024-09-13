bde0d9fca115fbd9d3be33cc2792721123d49570
manderz11 <rudisjauns@gmail.com>
2024-09-13 22:29:56 +0300

    Add uncommited handlers file

---
+++ Kits/Handlers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Server;
+using Exiled.Permissions.Extensions;
+using ExiledKitsPlugin.Classes;
+
+namespace ExiledKitsPlugin.Handlers;
+
+public class Handlers
+{
+    public void OnRoundEnded(RoundEndedEventArgs roundEndedEventArgs)
+    {
+        if (Plugin.Instance.Config.ResetCooldownsOnRoundEnd)
+        {
+            Plugin.Instance.KitCooldownManager = new KitCooldownManager();
+        }
+    }
+
+    public void SpawnedEvent(SpawnedEventArgs spawnedEventArgs)
+    {
+        Player p = spawnedEventArgs.Player;
+        // ignore cooldown bypassed players
+        if (p.CheckPermission("kits.give.cooldownbypass")) return;
+        foreach (var kitEntry in Plugin.Instance.KitCooldownManager.InitialCooldownKitEntries)
+        {
+            Plugin.Instance.KitCooldownManager.StartKitCooldown(kitEntry,p,kitEntry.InitialCooldown);
+            if (Plugin.Instance.Config.Debug) Log.Debug($"Starting kit cooldown for {kitEntry.Name} for {kitEntry.InitialCooldown} seconds");
+        }
+    }
+}
