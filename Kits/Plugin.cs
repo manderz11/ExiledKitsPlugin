c4ec8c16f3fc8031befeb2dbe4cf08467f18cc92
manderz11 <rudisjauns@gmail.com>
2024-12-23 16:39:27 +0200

    Version bump and NuGet package update

--- Kits/Plugin.cs
+++ Kits/Plugin.cs
@@ -9,7 +9,7 @@ namespace ExiledKitsPlugin
     {
         public override string Name => "Kits";
         public override string Author => "manderz11";
-        public override Version Version => new Version(1, 1, 4);
+        public override Version Version => new Version(1, 1, 5);
         public static Plugin Instance { get; set; }
         public KitEntryManager KitEntryManager;
         public KitManager KitManager;
